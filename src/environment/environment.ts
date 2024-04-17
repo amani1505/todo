acec87b7ce059d5439913b46571d09d3df94df41
amani1505 <amanideveloper1505@gmail.com>
2024-04-17 13:02:57 +0300

    Todo Drag and Drop completed

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
         endpoint: 'https://cloud.appwrite.io/v1',
         projectId: '661d50690c96565cb85a',
         dbId: '661e187e9a653ccc0265',
-        taskCollectionId:'661e4a9288b9e5926821',
+        taskCollectionId:'661f779037552e73a3fa',
         bucketId:'661e81aeba22f86934c6'
     },
     production: false,
